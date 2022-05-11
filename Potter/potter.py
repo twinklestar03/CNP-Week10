d520478be66e5769b548331f1e60439312d66c46
TwinkleStar03 <admin@t03.io>
2022-05-11 17:11:52 +0800

    Completed all test cases

--- Potter/potter.py
+++ Potter/potter.py
@@ -1,6 +1,9 @@
 from collections import Counter
 
 
+__all__ = ['Potter']
+
+
 class Potter:
     PER_BOOK = 8
     DISCOUNT_MAP = {
@@ -14,16 +17,23 @@ class Potter:
     def __init__(self, books=[]):
         self.books = books
 
-    def price(self) -> int:
+    def price(self, books=None) -> float:
+        if not books:
+            books = self.books
+            
         price = 0
-        while len(self.books) > 0:
-            counter = Counter(self.books)
-
-            if len(self.books) % 5 == 0 and len(counter.keys()) == 5:
+        while len(books) > 0:
+            counter = Counter(books)
+            if len(counter.keys()) == 5 and len(books) % 5 == 0:
                 # Maximum discount
                 price += self.PER_BOOK * len(counter.keys()) * (1 - self.DISCOUNT_MAP[len(counter.keys())])
                 for data, _ in counter.most_common():
-                    self.books.remove(data)
+                    books.remove(data)
+
+            elif (len(books) - 1) % 5 == 0 and len(counter.keys()) == 5:
+                # Dealing with 1 : 5 pair
+                price += 8
+                books.remove(counter.most_common()[0][0])
             else:
                 # Break books into groups that less than maximum discount
                 picked = 0
@@ -31,14 +41,12 @@ class Potter:
                     if picked >= 4:
                         break
                     picked += 1
-                    self.books.remove(data)
-                
+                    books.remove(data)
                 price += self.PER_BOOK * picked * (1 - self.DISCOUNT_MAP[picked])
 
-        print(price)
-        return 0
+        return price
 
 if __name__ == '__main__':
     potter = Potter([])
-    # potter.books = [1, 1, 2, 2, 3, 3, 4, 5]
+    potter.books = [0, 1, 1, 2, 3, 4]
     potter.price()
